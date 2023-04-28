3dc17445d8218def91f3e894a81574446d8741f3
mansooonuna <parkdasom95@gmail.com>
2023-04-28 19:08:51 +0900

    refactor : 조금 더 유연하게 ResponseDto를 사용할 수 있도록 개선

---
+++ src/main/java/com/example/hhblogdevelop/dto/GlobalResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.hhblogdevelop.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GlobalResponseDto {
+    private String message;
+    private int statusCode;
+
+    public GlobalResponseDto(String message, int statusCode) {
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+}
