54ebe8b32abaf47ee2089f25660ad277beab9deb
mansooonuna <parkdasom95@gmail.com>
2023-04-28 19:10:49 +0900

    fix : 스프링 시큐리티 적용안되어 있던 부분 수정

--- src/main/java/com/example/hhblogdevelop/controller/CommentController.java
+++ src/main/java/com/example/hhblogdevelop/controller/CommentController.java
@@ -1,15 +1,12 @@
 package com.example.hhblogdevelop.controller;
 
 import com.example.hhblogdevelop.dto.CommentRequestDto;
-import com.example.hhblogdevelop.dto.UserResponseDto;
-import com.example.hhblogdevelop.entity.Comment;
-
+import com.example.hhblogdevelop.dto.GlobalResponseDto;
+import com.example.hhblogdevelop.security.UserDetailsImpl;
 import com.example.hhblogdevelop.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api")
@@ -20,25 +17,25 @@ public class CommentController {
 
     // 댓글 작성
     @PostMapping("/comment")
-    public UserResponseDto<Comment> addComment(@RequestBody CommentRequestDto commentRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public GlobalResponseDto addComment(@RequestBody CommentRequestDto commentRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return commentService.addComment(commentRequestDto, userDetails.getUser());
     }
 
     // 댓글 수정
     @PutMapping("/comment/{id}")
-    public UserResponseDto<Comment> updateComment(@PathVariable Long id, @RequestBody CommentRequestDto commentRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public GlobalResponseDto updateComment(@PathVariable Long id, @RequestBody CommentRequestDto commentRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return commentService.updateComment(id, commentRequestDto, userDetails.getUser());
     }
 
     // 댓글 삭제
     @DeleteMapping("/comment/{id}")
-    public UserResponseDto<Comment> deleteComment(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public GlobalResponseDto deleteComment(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return commentService.deleteComment(id, userDetails.getUser());
     }
 
     // 댓글 좋아요
     @PutMapping("/comment/like/{id}")
-    public UserResponseDto<Comment> likeComment(@PathVariable Long id) {
+    public GlobalResponseDto likeComment(@PathVariable Long id) {
         return commentService.likeComment(id);
     }
 
