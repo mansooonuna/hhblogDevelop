cc8fe82ca7b83e7422ef20b048ca02f27ae34666
mansooonuna <parkdasom95@gmail.com>
2023-04-29 23:15:31 +0900

    feat : 회원탈퇴 기능 추가중 아직 오류 발생

--- src/main/java/com/example/hhblogdevelop/service/UserService.java
+++ src/main/java/com/example/hhblogdevelop/service/UserService.java
@@ -2,9 +2,9 @@ package com.example.hhblogdevelop.service;
 
 
 import com.example.hhblogdevelop.dto.GlobalResponseDto;
-import com.example.hhblogdevelop.dto.LoginRequestDto;
 import com.example.hhblogdevelop.dto.SignupRequestDto;
 import com.example.hhblogdevelop.dto.TokenDto;
+import com.example.hhblogdevelop.dto.UserRequestDto;
 import com.example.hhblogdevelop.entity.RefreshToken;
 import com.example.hhblogdevelop.entity.RoleType;
 import com.example.hhblogdevelop.entity.Users;
@@ -64,16 +64,16 @@ public class UserService {
 
     //로그인
     @Transactional
-    public GlobalResponseDto login(LoginRequestDto loginRequestDto, HttpServletResponse response) {
-        String username = loginRequestDto.getUsername();
-        String password = loginRequestDto.getPassword();
+    public GlobalResponseDto login(UserRequestDto userRequestDto, HttpServletResponse response) {
+        String username = userRequestDto.getUsername();
+        String password = userRequestDto.getPassword();
 
         // 사용자 확인
         Users user = userRepository.findByUsername(username).orElseThrow(
                 () -> new CustomException(USER_NOT_FOUND)
         );
         // 비밀번호 확인
-        if(!passwordEncoder.matches(password, user.getPassword())){
+        if (!passwordEncoder.matches(password, user.getPassword())) {
             return new GlobalResponseDto("비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST.value());
         }
 
@@ -83,7 +83,7 @@ public class UserService {
         //Refresh토큰 있는지 확인
         Optional<RefreshToken> refreshToken = refreshTokenRepository.findByUsername(username);
 
-        if(refreshToken.isPresent()){
+        if (refreshToken.isPresent()) {
             refreshTokenRepository.save(refreshToken.get().updateToken(tokenDto.getRefreshToken()));
         } else {
             RefreshToken newToken = new RefreshToken(tokenDto.getRefreshToken(), username);
@@ -94,9 +94,23 @@ public class UserService {
         return new GlobalResponseDto("정상적으로 로그인 되었습니다.", HttpStatus.OK.value());
     }
 
+    // 회원탈퇴
+    @Transactional
+    public GlobalResponseDto withdraw(UserRequestDto userRequestDto, Users user) {
+        Users findUser = userRepository.findByUsername(user.getUsername()).orElseThrow(
+                () -> new CustomException(USER_NOT_FOUND)
+        );
+        if (findUser.getPassword().equals(userRequestDto.getPassword())) {
+            userRepository.delete(findUser);
+            return new GlobalResponseDto("정상적으로 탈퇴 되었습니다. 감사합니다.", HttpStatus.OK.value());
+        } else {
+            throw new CustomException(INVALID_USER_PASSWORD);
+        }
+    }
+
     private void setHeader(HttpServletResponse response, TokenDto tokenDto) {
         response.addHeader(JwtUtil.ACCESS_KEY, tokenDto.getAccessToken());
         response.addHeader(JwtUtil.REFRESH_KEY, tokenDto.getRefreshToken());
     }
-
+    
 }
